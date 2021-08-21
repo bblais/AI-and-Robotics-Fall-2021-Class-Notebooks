894ac7223caf4ccbd663346c778c5232171d4e66
Brian Blais <bblais@gmail.com>
2021-08-21 11:11:06 -0400

    week 1 stuff

---
+++ Install and Update Packages.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Sprint #1
+
+# In[3]:
+
+
+get_ipython().system('pip install "git+git://github.com/bblais/Game" --upgrade')
+
+
+# ## Sprint #2
+
+# In[4]:
+
+
+get_ipython().system('pip install box2d')
+
+
+# In[5]:
+
+
+get_ipython().system('pip install "git+git://github.com/bblais/RobotSim373" --upgrade')
+
+
+# ## Sprint #3
+
+# In[ ]:
+
+
+
+
+
+# ## Sprint #4
+
+# In[1]:
+
+
+get_ipython().system('pip install scikit-learn')
+
+
+# In[1]:
+
+
+get_ipython().system('pip install "git+git://github.com/bblais/classy" --upgrade')
+
+
+# ## Sprint #5
+
+# In[2]:
+
+
+get_ipython().system('pip install "git+git://github.com/bblais/NumpyNet" --upgrade')
+
+
+# In[3]:
+
+
+get_ipython().system('pip install tqdm')
+
+
+# In[ ]:
+
+
+
+
