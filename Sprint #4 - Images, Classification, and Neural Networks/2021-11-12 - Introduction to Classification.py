3784e28c63215dd2cc522d72df80b8e596071d58
Brian Blais <bblais@gmail.com>
2021-11-17 08:47:41 -0500

    added making a prediction for images to the intro to classification notebook

--- Sprint #4 - Images, Classification, and Neural Networks/2021-11-12 - Introduction to Classification.py
+++ Sprint #4 - Images, Classification, and Neural Networks/2021-11-12 - Introduction to Classification.py
@@ -143,44 +143,44 @@ print("On the test set: ",C.percent_correct(data_test.vectors,data_test.targets)
 
 # ## Images
 
-# In[35]:
+# In[3]:
 
 
 images=image.load_images('images/digits')
 
 
-# In[39]:
+# In[4]:
 
 
 images=remap_targets(images,new_target_names=['0','1','2','3','4','5','6','7','8','9'])
 summary(images)
 
 
-# In[40]:
+# In[5]:
 
 
 data=image.images_to_vectors(images)
 
 
-# In[41]:
+# In[6]:
 
 
 data.targets
 
 
-# In[38]:
+# In[7]:
 
 
 data.target_names
 
 
-# In[42]:
+# In[8]:
 
 
 data_train,data_test=split(data)
 
 
-# In[43]:
+# In[9]:
 
 
 C=NaiveBayes()
@@ -192,13 +192,13 @@ print("On the training set: ",C.percent_correct(data_train.vectors,data_train.ta
 print("On the test set: ",C.percent_correct(data_test.vectors,data_test.targets))
 
 
-# In[45]:
+# In[10]:
 
 
 C.means.shape
 
 
-# In[49]:
+# In[11]:
 
 
 prototype=C.means[4,:]
@@ -206,17 +206,83 @@ print(prototype.shape)
 prototype
 
 
-# In[50]:
+# In[12]:
 
 
 prototype=prototype.reshape((8,8))
 
 
-# In[54]:
+# In[13]:
 
 
 imshow(prototype,cmap=cm.gray)
 colorbar()
+
+
+# ## making a prediction
+
+# In[15]:
+
+
+new_image=imread('images/digits/6/1734.png')
+imshow(new_image)
+
+
+# In[16]:
+
+
+new_image.shape
+
+
+# In[18]:
+
+
+new_vector=new_image.reshape(1,64)
+new_vector.shape
+
+
+# In[19]:
+
+
+C.predict(new_vector)
+
+
+# In[23]:
+
+
+target_number=C.predict(new_vector)[0]
+target_name=data_train.target_names[target_number]
+target_name
+
+
+# In[24]:
+
+
+new_vector
+
+
+# In[26]:
+
+
+data_train.vectors
+
+
+# yikes!   the data_train are between 0 and 255, and the vector is from 0 to 1!  No wonder it screwed up.
+
+# solution - rescale either the data, or the new vector
+
+# In[27]:
+
+
+new_vector=new_vector*255
+
+
+# In[28]:
+
+
+target_number=C.predict(new_vector)[0]
+target_name=data_train.target_names[target_number]
+target_name
 
 
 # In[ ]:
